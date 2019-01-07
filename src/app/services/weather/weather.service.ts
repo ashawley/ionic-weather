de2808e9ae91d7b750182d01d944b6ca0bb9df29
Aaron S. Hawley <aaron.s.hawley@gmail.com>
2019-01-07 16:50:19 -0500

    Basic configuration
    https://training-deck.firebaseapp.com/ionic-weather-v4-step07

--- src/app/services/weather/weather.service.ts
+++ src/app/services/weather/weather.service.ts
@@ -6,4 +6,12 @@ import { HttpClient } from '@angular/common/http';
 })
 export class WeatherService {
   constructor(private http: HttpClient) {}
+
+  private appId = '69f068bb8bf2bc3e061cb2b62c255c65'; // or use your own API key
+  private baseUrl = 'https://api.openweathermap.org/data/2.5';
+
+  private latitude = 38.969730;
+  private longitude = -77.383873;
+
+
 }
