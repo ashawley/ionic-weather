8fa07013ca8fa44904e08a2d272d39450089ac12
Aaron S. Hawley <aaron.s.hawley@gmail.com>
2019-01-07 16:48:08 -0500

    Inject the HTTP client
    https://training-deck.firebaseapp.com/ionic-weather-v4-step07

--- src/app/services/weather/weather.service.ts
+++ src/app/services/weather/weather.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
 }
