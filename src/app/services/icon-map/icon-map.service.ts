7bb1a513b6663a90898787d58350ab934dbcfccb
Aaron S. Hawley <aaron.s.hawley@gmail.com>
2019-01-07 15:34:56 -0500

    Modify the service
    https://training-deck.firebaseapp.com/ionic-weather-v4-step06

--- src/app/services/icon-map/icon-map.service.ts
+++ src/app/services/icon-map/icon-map.service.ts
@@ -4,6 +4,13 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class IconMapService {
-
-  constructor() { }
+  sunny = 'assets/images/sunny.png';
+  cloudy = 'assets/images/cloudy.png';
+  lightRain = 'assets/images/light-rain.png';
+  shower = 'assets/images/shower.png';
+  sunnyThunderStorm = 'assets/images/partial-tstorm.png';
+  thunderStorm = 'assets/images/tstorm.png';
+  fog = 'assets/images/fog.png';
+  snow = 'assets/images/snow.png';
+  unknown = 'assets/images/dunno.png';
 }
