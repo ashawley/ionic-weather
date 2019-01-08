b619a2834cebc395f3d80a21bf644cd2238b0d39
Aaron S. Hawley <aaron.s.hawley@gmail.com>
2019-01-08 10:46:12 -0500

    Transforming the weather
    https://training-deck.firebaseapp.com/ionic-weather-v4-step07

--- src/app/services/weather/weather.service.ts
+++ src/app/services/weather/weather.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { Forecast } from '../../models/forecast';
+import { Weather } from '../../models/weather';
+import { UVIndex } from '../..//models/uv-index';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -13,11 +20,74 @@ export class WeatherService {
   private latitude = 38.969730;
   private longitude = -77.383873;
 
-  current(): Observable<any> {
+  current(): Observable<Weather> {
     return this.http.get(
       `${this.baseUrl}/weather?lat=${this.latitude}&lon=${
         this.longitude
-      }&appid=${this.appId}`);
+      }&appid=${this.appId}`
+      ).pipe(map(res => this.unpackWeather(res)));
   }
 
+  forecast(): Observable<Forecast> {
+    return this.http.get(
+      `${this.baseUrl}/forecast?lat=${this.latitude}&lon=${
+        this.longitude
+      }&appid=${this.appId}`
+      ).pipe(map(res => this.unpackForecast(res)));
+  }
+
+  uvIndex(): Observable<UVIndex> {
+    return this.http.get(
+      `${this.baseUrl}/uvi?lat=${this.latitude}&lon=${
+        this.longitude
+      }&appid=${this.appId}`
+      ).pipe(map(res => this.unpackUvIndex(res)));
+  }
+
+  private unpackWeather(res: any): Weather {
+    return {
+      temperature: res.main.temp,
+      condition: res.weather[0].id,
+      date: new Date(res.dt * 1000)
+    };
+  }
+
+  private unpackForecast(res: any): Forecast {
+    let currentDay: Array<Weather>;
+    let prevDate: number;
+    const forecast: Forecast = [];
+
+    res.list.forEach(item => {
+      const w = this.unpackWeather(item);
+      if (w.date.getDate() !== prevDate) {
+        prevDate = w.date.getDate();
+        currentDay = [];
+        forecast.push(currentDay);
+      }
+      currentDay.push(w);
+    });
+
+    return forecast;
+  }
+
+
+  private unpackUvIndex(res: any): UVIndex {
+    let riskValue: number = res.value;
+    let calculatedLevel: number;  
+    if (riskValue < 3) {
+      calculatedLevel = 0;
+    } else if (3 <= riskValue && riskValue < 6) {
+      calculatedLevel = 1;
+    } else if (6 <= riskValue && riskValue < 8) {
+      calculatedLevel = 2;
+    } else if (8 <= riskValue && riskValue < 11) {
+      calculatedLevel = 3;
+    } else if (riskValue >= 11) {
+      calculatedLevel = 4;
+    }
+    return {
+      value: riskValue,
+      riskLevel: calculatedLevel
+    };
+  }
 }
