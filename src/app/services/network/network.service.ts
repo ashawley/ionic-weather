82e1af03cbbb165db6f779cfb44b7bdab2555848
Aaron S. Hawley <aaron.s.hawley@gmail.com>
2019-01-09 22:19:38 -0500

    Create a network service
    https://training-deck.firebaseapp.com/ionic-weather-v4-step12

--- src/app/services/network/network.service.ts
+++ src/app/services/network/network.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { Network } from '@ionic-native/network/ngx';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NetworkService {
+  constructor(
+    private network: Network,
+    private platform: Platform
+  ) {}
 
-  constructor() { }
+  get onLine(): boolean {
+    return this.platform.is('cordova')
+      ? !!this.network.type
+        && this.network.type.toLowerCase() !== 'unknown'
+        && this.network.type.toLowerCase() !== 'none'
+      : navigator.onLine;
+  }
 }
